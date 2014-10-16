c2d6efd9d17dc7f3260ae12478717ae5cf02aa7f
Rob Halff <rob.halff@gmail.com>
2014-10-16 15:40:38 +0200

    update

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
+'use strict';
+
 var sjc = require('strip-json-comments');
 var glob = require('glob');
 var fs = require('fs');
-var schema = sjc(fs.readFileSync('./schema-form.json').toString());
+var schema = JSON.parse(sjc(fs.readFileSync('./schema-form.json').toString()));
 var tv4 = require('tv4');
 
 var g = glob('fixtures/*.json');
-var debug = process.env.DEBUG;
-
+//var debug = process.env.DEBUG;
 g.on('match', function(file) {
 
   console.log('require:', file);
-  var json = sjc(fs.readFileSync('./' + file).toString());
+  var json = JSON.parse(sjc(fs.readFileSync('./' + file).toString()));
+  console.log(Object.keys(json), Object.keys(schema));
   var res = tv4.validate(json, schema);
 
-  if(tv4.error) {
+  if(!res) {
+	  
+		//console.log(JSON.parse(JSON.stringify(tv4.error)));
+	  
     var idx = tv4.error.dataPath.split('').splice(1).join('');
 
     console.log(file + ':', tv4.error.message, {
       dataPath: tv4.error.dataPath,
       schemaPath: tv4.error.schemaPath
     }, json[idx]);
-
+    
     process.exit();
   } else {
     console.log(file + ': OK');
   }
 
-  tv4.reset();
+  //tv4.reset();
 
 
 });
